82a512a779bb7d4fe8f0612dc986903c9ec955bb
Matthew O'Connor <moconnor16@gmail.com>
2016-10-13 15:52:16 -0500

    load kmldatasource once and store in client

--- map/static/map/js/kmlmap-viewer.js
+++ map/static/map/js/kmlmap-viewer.js
@@ -3,12 +3,23 @@ $(function(){
 	var viewer = new Cesium.Viewer("worldmap");
 
 	$("#maplist .selectable-list").on("click", "a", function(event){
-		var url = $(event.target).attr("data-source")
-		viewer.dataSources.add(Cesium.KmlDataSource.load(
-			url, {
-	        	camera: viewer.camera,
-	        	canvas: viewer.canvas
-	        }));
+		var dataSource = $(event.target).data("dataSource");
+
+		if(dataSource) {
+			viewer.flyTo(dataSource.entities);
+		} else {
+			var url = $(event.target).attr("data-source");
+			dataSource = Cesium.KmlDataSource.load(
+				url, {
+		        	camera: viewer.camera,
+		        	canvas: viewer.canvas
+		        });
+			viewer.dataSources.add(dataSource).then( function (kmlData) { //success
+				$(event.target).data("dataSource", kmlData);
+                viewer.flyTo(kmlData.entities);
+            });
+		}
+
 	});
 
 });
